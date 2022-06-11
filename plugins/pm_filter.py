da71171469476a71d0bff94e3e2cba86b8c60b53
basilozr <106648030+basilozr@users.noreply.github.com>
2022-06-11 15:39:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -418,7 +418,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-        await query.answer('Piracy Is Crime')
+        await query.answer('ബെർതെ ഒരുരസം')
     elif query.data == "help":
         buttons = [[
             InlineKeyboardButton('🏠 Home', callback_data='start'),
